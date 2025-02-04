f537d067a005ad009b07485c578f8279c7518830
k k k <kiisystem@naver.com>
2025-02-04 23:44:42 +0900

    Update router.ex

--- lib/ret_web/router.ex
+++ lib/ret_web/router.ex
@@ -95,9 +95,12 @@ defmodule RetWeb.Router do
   end
 
   scope "/api/postgrest" do
-    pipe_through [:secure_headers, :auth_required, :admin_required, :proxy_api]
-
-    forward "/", RetWeb.Plugs.PostgrestProxy
+     # pipe_through [:secure_headers, :auth_required, :admin_required, :proxy_api]
+     if(Mix.env() == :prod) do
+     pipe_through([:secure_headers])
+     end
+  
+     forward "/", RetWeb.Plugs.PostgrestProxy
   end
 
   scope "/api/ita" do
